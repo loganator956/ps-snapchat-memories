3cf9a250fffbb321a48beb62febae3cb6832721b
loganator956 <65763483+loganator956@users.noreply.github.com>
2023-09-03 14:22:35 +0100

    add exiftool

--- download.ps1
+++ download.ps1
@@ -9,11 +9,12 @@ foreach ($file in $data.'Saved Media') {
     $date = $file.Date.Replace(" UTC", "")
     $RealURI = Invoke-WebRequest -Uri $jsonLink -Method Post -Headers @{"Content-Type" = "application/x-www-form-urlencoded" }
     $RealURI = $RealURI.Content
-    $RealURI -match '(?:.)*\/(.*\.\w{3,4})(?:\?)'
+    $RealURI -match '(?:.)*\/(.*\.\w{3,4})(?:\?)' | Out-Null
     $fileName = $Matches[1]
     Invoke-WebRequest -Uri $RealURI -OutFile "memories\$fileName"
     $DownloadedFile = Get-Item -Path "memories\$fileName"
     $DownloadedFile.CreationTimeUtc = $date
     $DownloadedFile.LastWriteTimeUtc = $date
     Write-Host "Downloaded $fileName"
+    exiftool "-AllDates<filemodifydate" $DownloadedFile.FullName
 }
