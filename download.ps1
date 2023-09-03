a5ea0e55d9a7d4e8f19168784205e85e9de58423
loganator956 <65763483+loganator956@users.noreply.github.com>
2023-09-03 11:39:28 +0100

    add downloading change file dates

---
+++ download.ps1
@@ -0,0 +1,19 @@
+$data = Get-Content -Path "memories_history.json" | ConvertFrom-Json
+
+if ((Test-Path -Path "memories") -eq $false) {
+    New-Item -Path "memories" -ItemType Directory
+}
+
+foreach ($file in $data.'Saved Media') {
+    $jsonLink = $file.'Download Link'
+    $date = $file.Date.Replace(" UTC", "")
+    $RealURI = Invoke-WebRequest -Uri $jsonLink -Method Post -Headers @{"Content-Type" = "application/x-www-form-urlencoded" }
+    $RealURI = $RealURI.Content
+    $RealURI -match '(?:.)*\/(.*\.\w{3,4})(?:\?)'
+    $fileName = $Matches[1]
+    Invoke-WebRequest -Uri $RealURI -OutFile "memories\$fileName"
+    $DownloadedFile = Get-Item -Path "memories\$fileName"
+    $DownloadedFile.CreationTimeUtc = $date
+    $DownloadedFile.LastWriteTimeUtc = $date
+    Write-Host "Downloaded $fileName"
+}
